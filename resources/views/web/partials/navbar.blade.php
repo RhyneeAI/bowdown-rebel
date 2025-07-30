b57dab4ab187ba5899e29b05d0ed558beb2d6c8a
nandamaull244 <110996947+nandamaull244@users.noreply.github.com>
2025-07-30 20:33:21 +0700

    enhance : upsize font navbar web

--- resources/views/web/partials/navbar.blade.php
+++ resources/views/web/partials/navbar.blade.php
@@ -4,28 +4,20 @@
 		            <div class="col-md-3 col-xs-2">
 		                <div id="fh5co-logo"><a href="index.html">Bowdown Rebel</a></div>
 		            </div>
-		            <div class="col-md-5 col-xs-6 text-center menu-1">
+		            <div class="col-md-5 col-xs-7 text-center menu-1">
 		                <ul>
-		                    <li><a href="{{ route('home.index') }}">Home</a></li>
+		                    <li><a href="{{ route('home.index') }}" style="font-size:1.2em">Home</a></li>
 		                    <li><a
-		                            href="{{ $role != null ? route($role.'.shop.index') : route('shop.index') }}">Shop</a>
+		                            href="{{ $role != null ? route($role.'.shop.index') : route('shop.index') }}" style="font-size:1.2em">Shop</a>
 		                    </li>
-		                    <li><a href="{{ route('about') }}">About</a></li>
+		                    <li><a href="{{ route('about') }}" style="font-size:1.2em">About</a></li>
 		                </ul>
 		            </div>
-		            <div class="col-md-4 col-xs-4 text-right hidden-xs menu-2">
+		            <div class="col-md-4 col-xs-3 text-right hidden-xs menu-2">
 		                <ul>
-							{{-- <li class="search">
-								<div class="input-group">
-									<input type="text" placeholder="Search..">
-		                            <span class="input-group-btn">
-										<button class="btn btn-primary" type="button"><i class="icon-search"></i></button>
-		                            </span>
-		                        </div>
-		                    </li> --}}
 							@auth('User')
 								<li class="has-dropdown">
-		                            <i class="icon-user"></i><a href="{{ route($role.'.profile.index') }}">Profile</a>
+		                            <i class="icon-user"></i><a href="{{ route($role.'.profile.index') }}" style="font-size:1.2em">Profile</a>
 		                            <ul class="dropdown">
 		                                <li><a href="{{ route($role.'.profile.index') }}">{{ auth('User')->user()->nama }}
 		                                    </a></li>
@@ -49,10 +41,10 @@
 							@endauth	
 							@guest('User')
 								<li class="user">
-									<a href="{{ route('auth.login') }}" class="btn-user">Login</a>
+									<a href="{{ route('auth.login') }}" class="btn-user"style="font-size:1.2em">Login</a>
 								</li>
 								<li class="shopping-cart">
-									<a href="{{ $role != null ? route($role.'.cart.index') : route('cart.index') }}" class="cart">
+									<a href="{{ $role != null ? route($role.'.cart.index') : route('cart.index') }}" class="cart" style="font-size:1.2em">
 										<span>
 											<small>0</small>
 											<i class="icon-shopping-cart"></i>
